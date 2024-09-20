b69509871ed6566527d08fa2876fe2b859bf0a53
Simon Nair <si.nair205@gmail.com>
2024-09-19 22:28:42 -0400

    setup lexer

--- lex.py
+++ lex.py
@@ -1,1 +1,17 @@
-print("hello")
+class Lexer:
+    def __init__(self, source):
+        pass
+
+    # Process the next character
+    def nextChar(self):
+        pass
+
+    # Return the lookahead character
+    def peek(self):
+        pass
+
+    # Invalid token found, print error message and exit
+    def abort(self, message):
+        pass
+
+    
