5a65e85a97d381f3bb963cb256ae55e64ccfb69b
Simon Nair <si.nair205@gmail.com>
2024-09-25 22:19:06 -0400

    fix

--- parse.py
+++ parse.py
@@ -9,26 +9,46 @@ class Parser:
         self.nextToken()
         self.nextToken()  # call twice to init current and peek
 
-        # Return true if the current token matches
-        def checkToken(self, kind):
-            return kind == self.curToken.kind
+    # Return true if the current token matches
+    def checkToken(self, kind):
+        return kind == self.curToken.kind
 
-        # Return true if the next token matches
-        def checkPeek(self, kind):
-            return kind == self.peekToken.kind
+    # Return true if the next token matches
+    def checkPeek(self, kind):
+        return kind == self.peekToken.kind
 
-        # Try to match current token. If not, error. Advances the current token
-        def match(self, kind):
-            if not self.checkToken(kind):
-                self.abort("Expected " + kind.name + ", got " + self.curToken.kind.name)
+    # Try to match current token. If not, error. Advances the current token
+    def match(self, kind):
+        if not self.checkToken(kind):
+            self.abort("Expected " + kind.name + ", got " + self.curToken.kind.name)
+        self.nextToken()
+
+    # Advances the current token
+    def nextToken(self):
+        self.curToken = self.peekToken
+        self.peekToken = self.lexer.getToken()
+        # lexer handles passing EOF
+
+    def abort(self, message):
+        sys.exit("Error. " + message)
+
+    # Production Rules
+
+    # program ::= {statement}
+    def program(self):
+        print("PROGRAM")
+
+        # Parse all statements in the program
+        while not self.checkToken(TokenType.EOF):
+            self.statement()
+
+    def statement(self):
+        # Check first token to see what kind of statement this is
+
+        # "PRINT" (expression | string)
+        if self.checkToken(TokenType.PRINT):
+            print("STATEMENT-PRINT")
             self.nextToken()
 
-        # Advances the current token
-        def nextToken(self):
-            self.curToken = self.peekToken
-            self.peekToken = self.lexer.getToken()
-            # lexer handles passing EOF
-
-        def abort(self, message):
-            sys.exit("Error. " + message)
+        
  
